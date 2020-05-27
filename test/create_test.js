e74cec2fc9edd220b477cdb3ef129a41c276585b
AlaguSelvan <alaguselvan97@gmail.com>
2020-05-27 08:52:55 +0530

    complete

--- test/create_test.js
+++ test/create_test.js
@@ -3,9 +3,14 @@ const User = require('../src/user');
 
 
 describe ('Creating records', () => {
-    it('saves a user', () => {
+    it('saves a user', (done) => {
         const joe = new User({ name: 'Joe' })
 
-        joe.save();
+        joe.save()
+            .then(()=>{
+                // Has Joe been saved successfully
+                assert(!joe.isNew);
+                done(); 
+            })
     });
 });
